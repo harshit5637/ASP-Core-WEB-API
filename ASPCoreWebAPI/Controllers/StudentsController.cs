f081a34ff8bb31e9365c730de301b93d51f03ef7
apnaharshit-demo <hm2417871@gmail.com>
2024-03-07 10:45:16 +0530

    Asp Core Web API

---
+++ ASPCoreWebAPI/Controllers/StudentsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPCoreWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        public List<string> Students = new List<string>(){
+            "Harshit",
+            "Mishra",
+            "Abhinav",
+            "Avinash",
+            "Anubhav"
+        };
+        [HttpGet]
+        public List<string> GetStudents()
+        {
+            return Students;
+        }
+        [HttpGet("{id}")]
+        public string GetStudentsByIndex(int id)
+        {
+            return Students.ElementAt(id);
+        }
+
+    }
+}
